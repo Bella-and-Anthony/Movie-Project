63dff08ba7c002e96eb44bc7203598f4b0c718d3
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-10 10:52:34 -0600

    setTimeout for loading and fetch

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 "use strict";
 
 // GETTING MOVIES AFTER LOADING
-fetch("https://rocky-enchanting-wineberry.glitch.me/movies")
-    .then(res => res.json())
-    // .then(movies => console.log(movies))
-    .then(function(movies) {
-        console.log(movies)
+setTimeout(() => {
+    $("#spinner").addClass("loading")
+}, 2000)
+setTimeout(() => {
+    fetch("https://rocky-enchanting-wineberry.glitch.me/movies")
+        .then(res => res.json())
+        .then(function(movies) {
+            console.log(movies)
             for(let i = 0; i <= movies.length; i++) {
                 let movieData =`<div class="card" style="width: 18rem;">
                                     <div class="card-body">
@@ -26,7 +29,33 @@ fetch("https://rocky-enchanting-wineberry.glitch.me/movies")
                                 </div>`
                 $("#displayMovies").append(movieData)
             }
-    })
+        })
+}, 2000)
+// fetch("https://rocky-enchanting-wineberry.glitch.me/movies")
+//     .then(res => res.json())
+//     .then(function(movies) {
+//         console.log(movies)
+//             for(let i = 0; i <= movies.length; i++) {
+//                 let movieData =`<div class="card" style="width: 18rem;">
+//                                     <div class="card-body">
+//                                         <div class="card-title">${movies[i].title}</div>
+//                                         <div></div>
+//                                         <div class="card=text">${movies[i].plot}</div>
+//                                     </div>
+//                                     <ul class="list-group list-group-flush">
+//                                         <li class="list-group-item">${movies[i].actors}</li>
+//                                         <li class="list-group-item">${movies[i].director}</li>
+//                                         <li class="list-group-item">${movies[i].genre}</li>
+//                                         <li class="list-group-item">${movies[i].year}</li>
+//                                     </ul>
+//                                     <div class="card-body">
+//                                         <a class="card-link btn btn-primary" href=""></a>
+//                                         <a class="card-link btn btn-primary" href=""></a>
+//                                     </div>
+//                                 </div>`
+//                 $("#displayMovies").append(movieData)
+//             }
+//     })
 
 
 
