ef699f3aa0a788b9a55628d72c462d6b9219af80
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-02-10 09:55:15 -0600

    first commit added cards to hold appended info

--- script.js
+++ script.js
@@ -7,8 +7,26 @@ fetch("https://rocky-enchanting-wineberry.glitch.me/movies")
     .then(function(movies) {
         console.log(movies)
             for(let i = 0; i <= movies.length; i++) {
-                let movieData =
-                    `<div>${movies[i].title}</div>`
+                let movieData =`<div class="card" style="width: 18rem;">
+                                    <div class="card-body">
+                                        <div class="card-title">${movies[i].title}</div>
+                                        <div></div>
+                                        <div class="card=text">${movies[i].plot}</div>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">${movies[i].actors}</li>
+                                        <li class="list-group-item">${movies[i].director}</li>
+                                        <li class="list-group-item">${movies[i].genre}</li>
+                                        <li class="list-group-item">${movies[i].year}</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <a class="card-link btn btn-primary" href=""></a>
+                                        <a class="card-link btn btn-primary" href=""></a>
+                                    </div>
+                                </div>`
                 $("#displayMovies").append(movieData)
             }
     })
+
+
+
